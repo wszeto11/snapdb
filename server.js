892a9b32b504e8f164c46e720d6b4ade2f8adbd4
Willard Szeto <wszeto11@gmail.com>
2023-02-14 14:53:07 -0500

    Middleware built, local server is able to connect in Insomnia

--- server.js
+++ server.js
@@ -1,9 +1,18 @@
 const express = require('express')
 const cors = require('cors')
 const logger = require('morgan')
-const PORT = process.env.PORT || 3001
 
 const app = express()
+
+const PORT = process.env.PORT || 3001
+app.use(cors())
+app.use(logger('dev'))
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+app.get('/', (request, response) => {
+  response.send({ msg: 'Server Running' })
+})
 
 app.listen(PORT, () => {
   console.log(`Express server listening on port ${PORT}`)
