e31c23ac4ab3f6c7541923db4f7f7709c24ed3b1
Willard Szeto <wszeto11@gmail.com>
2023-02-15 09:39:03 -0500

    All No Ability cards added

--- seed/cards.js
+++ seed/cards.js
@@ -13,11 +13,15 @@ const main = async () => {
   const destroy = await Type.find({ name: 'Destroy' })
 
   const cards = [
+    // On Reveal
+
+    // Ongoing
+
     // No Ability
     {
       name: 'Abomination',
-      cost: [5],
-      power: [9],
+      cost: '5',
+      power: '9',
       type: 'No Ability',
       type_id: noAbility[0]._id,
       image:
@@ -25,8 +29,8 @@ const main = async () => {
     },
     {
       name: 'Cyclops',
-      cost: [3],
-      power: [4],
+      cost: '3',
+      power: '4',
       type: 'No Ability',
       type_id: noAbility[0]._id,
       image:
@@ -34,8 +38,8 @@ const main = async () => {
     },
     {
       name: 'Hulk',
-      cost: [6],
-      power: [12],
+      cost: '6',
+      power: '12',
       type: 'No Ability',
       type_id: noAbility[0].id,
       image:
@@ -43,13 +47,46 @@ const main = async () => {
     },
     {
       name: 'Misty Knight',
-      cost: [1],
-      power: [2],
+      cost: '1',
+      power: '2',
       type: 'No Ability',
       type_id: noAbility[0].id,
       image:
         'https://marvelsnapzone.com/wp-content/themes/blocksy-child/assets/media/cards/misty-knight.webp?v=39'
+    },
+    {
+      name: 'Shocker',
+      cost: '1',
+      power: '3',
+      type: 'No Ability',
+      type_id: noAbility[0].id,
+      image:
+        'https://marvelsnapzone.com/wp-content/themes/blocksy-child/assets/media/cards/shocker.webp?v=39'
+    },
+    {
+      name: 'Thing',
+      cost: '4',
+      power: '6',
+      type: 'No Ability',
+      type_id: noAbility[0].id,
+      image:
+        'https://marvelsnapzone.com/wp-content/themes/blocksy-child/assets/media/cards/the-thing.webp?v=39'
+    },
+    {
+      name: 'Wasp',
+      cost: '0',
+      power: '1',
+      type: 'No Ability',
+      type_id: noAbility[0].id,
+      image:
+        'https://marvelsnapzone.com/wp-content/themes/blocksy-child/assets/media/cards/wasp.webp?v=39'
     }
+
+    // Discard
+
+    // Move
+
+    // Destroy
   ]
 
   await Card.insertMany(cards)
