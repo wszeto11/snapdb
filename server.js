067c09355ac4146a8daad03c611022e7671a3406
Willard Szeto <wszeto11@gmail.com>
2023-02-16 11:53:00 -0500

    updated server.js with additional routes

--- server.js
+++ server.js
@@ -5,15 +5,12 @@ const bodyParser = require('body-parser')
 const logger = require('morgan')
 const PORT = process.env.PORT || 3001
 const { default: mongoose } = require('mongoose')
-require('dotenv/config')
 const cors = require('cors')
+const controllers = require('./controllers')
 const { MongoClient } = require('mongodb')
 const { Card } = require('./models')
 const { Ability } = require('./models')
 const { Deck } = require('./models')
-// require() imports and middleware here ^
-
-// const PORT = process.env.PORT || 3023;
 
 const app = express()
 
@@ -22,10 +19,9 @@ app.use(bodyParser.json())
 app.use(logger('dev'))
 app.use(express.json())
 // app.use(express.static(`${__dirname}/client/build`))
-// app.use() middleware here ^
 
 // app.use('/api')
-// app.use(routes)
+
 db.on('error', console.error.bind(console, 'MongoDB connection error:'))
 
 // DB CONNECTION //
@@ -41,25 +37,72 @@ mongoose
     console.log(err)
   })
 
+// Routes
+app.get('/', (req, res) => {
+  res.send('This is Snap!')
+})
+
+// Cards routes
 app.get('/cards', async (req, res) => {
   const cards = await Card.find({})
   res.json(cards)
 })
 
+app.get('/cards/:id', async (req, res) => {
+  try {
+    const { id } = req.params
+    const card = await Card.findById(id)
+    if (!card) throw Error('Card not found')
+    res.json(card)
+  } catch (e) {
+    console.log(e)
+    res.send('Card not found!')
+  }
+})
+
+app.delete('/cards/:id', controllers.deleteCard)
+
+// Abilities routes
 app.get('/abilities', async (req, res) => {
   const abilities = await Ability.find({})
   res.json(abilities)
 })
 
+app.get('/abilities/:id', async (req, res) => {
+  try {
+    const { id } = req.params
+    const ability = await Ability.findById(id)
+    if (!ability) throw Error('Ability not found')
+    res.json(ability)
+  } catch (e) {
+    console.log(e)
+    res.send('Ability not found!')
+  }
+})
+
+app.delete('/abilities/:id', controllers.deleteAbility)
+
+// Deck routes
 app.get('/decks', async (req, res) => {
   const decks = await Deck.find({})
   res.json(decks)
 })
 
-app.get('/', (req, res) => {
-  res.send('This is root!')
+app.get('/decks/:id', async (req, res) => {
+  try {
+    const { id } = req.params
+    const deck = await Deck.findById(id)
+    if (!deck) throw Error('Deck not found')
+    res.json(deck)
+  } catch (e) {
+    console.log(e)
+    res.send('Deck not found!')
+  }
 })
 
-app.listen(PORT, () => {
-  console.log(`Listening on port: ${PORT}`)
-})
+app.delete('/abilities/:id', controllers.deleteAbility)
+// const PORT = process.env.PORT || 3001
+// app.get('/*', (req, res) => {
+//   res.sendFile(`${__dirname}/client/build/index.html`)
+// })
+app.listen(PORT, () => console.log(`Listening on port: ${PORT}`))
